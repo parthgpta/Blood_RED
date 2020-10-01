419bc7d809e8fd0bec6effab5a81b22182389465
Shreya Gupta <51705629+shryagupta@users.noreply.github.com>
2020-10-01 17:35:46 +0530

    Update settings.py

--- Bloodred/settings.py
+++ Bloodred/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
